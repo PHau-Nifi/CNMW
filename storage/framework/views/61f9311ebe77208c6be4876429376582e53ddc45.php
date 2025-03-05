14b197f19b067133c4edb0525a4860a5620ecf16
PHau-Nifi <nguyenmaiphuchau2002@gmail.com>
2025-03-05 23:57:45 +0700

    Add resource

--- storage/framework/views/61f9311ebe77208c6be4876429376582e53ddc45.php
+++
@@ -1,6 +0,0 @@
-<h3><?php echo e($account->fullName); ?></h3>
-<p>idahijew</p>
-
-<p>
-    <a href="">click</a>
-</p><?php /**PATH D:\LVTN\QLRapPhim\resources\views/email/index.blade.php ENDPATH**/ ?>
